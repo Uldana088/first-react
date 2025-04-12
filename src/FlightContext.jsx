e5d7008b7f4892c18606d14c979df2ab214f7c3f
Uldana088 <abdiomaruldana2@gmail.com>
2025-04-12 11:06:54 +0500

    FlightContext.jsx
    Home wrk 30/4

---
+++ src/FlightContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useState } from "react";
+
+export const FlightContext = createContext();
+export const FlightProvider = ({ children }) => {
+  const [flights, setFlights] = useState([]);
+  const [bookings, setBookings] = useState([]);
+
+  return (
+    <FlightContext.Provider value={{
+      flights, setFlights,
+      bookings, setBookings
+    }}>
+      {children}
+    </FlightContext.Provider>
+  );
+};
